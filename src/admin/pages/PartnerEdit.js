56e3eb4a69108bd7e059d3612c79eddc245f8f0f
techiemithlesh <techie.mithlesh@gmail.com>
2024-02-25 01:20:13 +0530

    component created for all page edit

---
+++ src/admin/pages/PartnerEdit.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../layouts/Layout";
+
+
+const PartnerEdit = () => {
+    return (
+        <Layout>
+            <h1>Partner Edit</h1>
+        </Layout>
+    )
+}
+
+
+export default PartnerEdit;
---
+++ src/admin/pages/PolicyEdit.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../layouts/Layout";
+import Card from "../Components/Card";
+
+
+const PolicyEdit = () => {
+    return (
+        <Layout>
+            <Card>
+                <h1>Policy Edit</h1>
+            </Card>
+        </Layout>
+    )
+}
+
+export default PolicyEdit;
